67be6fdfbbc7133857e4d5287f2aef5dcb6c256a
yomesh-dk <yomeshofficial@gmail.com>
2023-11-21 21:32:24 +0530

    brocode test

--- bro-code/test.c
+++ bro-code/test.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 int main(){
-    char name[]= "Bande";
-    int age = 64;
-    char assigned_letter = 'Z';
-    float height = 6.3;
+    char name[]="Loba Loba";
+    int age = 25;
+    float average = 4.2;
+    char grade = 'C';
 
-    printf("%s is %d years old.He is %fft tall and live in street %c",name,age,height,assigned_letter);
+    printf("%s is %d years old, and his grade is %c . and his average is %f",name,age,grade,average);
+
+    return 0;
 }
